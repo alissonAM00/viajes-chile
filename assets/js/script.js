2340057e16de848a2d8cf91691bbf27bb4f27bdc
alisson <alissonavalos@gmail.com>
2024-03-25 20:15:18 -0300

    inicio proyecto viajes chile

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
+// Tooltip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+//alerts
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('gracias! mensaje enviado correctamente!', 'success')
+  })
+}
